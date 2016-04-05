8916f6fc136545165035571ac14f58d915e7ccec
Fran Rios <fcojriosbello@gmail.com>
2016-04-05 11:52:40 +0000

    Hash integration

--- src/druid-master.rb
+++ src/druid-master.rb
@@ -1,13 +1,41 @@
 require_relative 'tail_f'
+#$mutex = Mutex.new
 
-threads = []
+$metrics = []
 
-Dir.glob('/var/log/druid/*.log') do |log_file|
-  puts "Started At #{Time.now} with file: " + log_file
-  t = Thread.new do
-  log_handler(log_file)
+def druid_master
+  threads = []
+  $i=1
+  Dir.glob('/var/log/druid/*.log') do |log_file|
+    puts "Started At #{Time.now} with file: " + log_file
+    t = Thread.new do
+      log_handler(log_file)
+    end
+    threads << t
   end
-  threads << t
+  threads.each { |thread| thread.join }
 end
 
-threads.each { |thread| thread.join }
+def recolector
+  $metrics.each do |metric|
+    puts $metrics.size
+    +  'This: '
+    + metric[:metric] + ' '
+    + metric[:value] + ' '
+    + metric[:ttl]
+  end
+end
+items = []
+one_at_a_time = Mutex.new
+
+# Show the values every 5 seconds
+Thread.new do
+  i=1
+  loop do
+    puts '------------------------- i = ' + i.to_s
+    recolector
+    sleep 5
+    i=i+1
+  end
+end
+druid_master
--- src/tail_f.rb
+++ src/tail_f.rb
@@ -2,10 +2,28 @@ require 'file-tail'
 
 def druid_parser(line, file)
   unless line.nil?
-    service = line.match(/"service":"(.*)","host/)[1] unless line.match(/"metric":"(.*)","value/).nil?
-    metric = line.match(/"metric":"(.*)","value/)[1] unless line.match(/"metric":"(.*)","value/).nil?
-    value = line.match(/"value":(\d+),"/)[1] unless line.match(/"value":(\d+),"/).nil?
-      puts file + "Service is #{service}, Metric is #{metric} and its value is #{value}" unless (service.nil? || value.nil?)
+    unless line.match(/"metric":"(.*)","value/).nil?
+      service = line.match(/"service":"(.*)","host/)[1]
+    end
+    unless line.match(/"metric":"(.*)","value/).nil?
+      metric = line.match(/"metric":"(.*)","value/)[1]
+    end
+    unless line.match(/"value":(\d+),"/).nil?
+      value = line.match(/"value":(\d+),"/)[1]
+    end
+    #  unless service.nil? || value.nil?
+    #    puts file + "Service is #{service},
+    #     Metric is #{metric} and its value is #{value}"
+    #  end
+    unless service.nil? || value.nil?
+        $metrics << {
+          metric: metric,
+          service: service,
+          value: value,
+          ttl: 3
+        }
+    end
+    #    puts 'metric added'
   end
 end
 
@@ -13,6 +31,8 @@ def log_handler(filename)
   File.open(filename, 'r') do |log|
     log.extend(File::Tail)
     log.backward(1)
+    #  puts 'here' + $i.to_s
+    $i=$i+1
     log.tail { |line| druid_parser(line, filename) }
   end
 end
