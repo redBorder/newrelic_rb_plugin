00222a9d3e3f5e12db76988650145ed562e1019d
Fran Rios <fcojriosbello@gmail.com>
2016-04-26 08:40:40 +0000

    added middleManager files

--- bin/druid-master.rb
+++ bin/druid-master.rb
@@ -9,6 +9,13 @@ def druid_master
     end
     threads << t
   end
+  Dir.glob('/tmp/druid-indexing/persistent/tasks/index_*/log') do |log_file|
+    puts "Started At #{Time.now} with file: " + log_file
+    t = Thread.new do
+      log_handler(log_file)
+    end
+    threads << t
+  end
 end
 
 def recolector
