b875dcfb1afb835754eedef762e4e29d2732e4a7
root <root@rbp777cg5qri.(none)>
2016-05-11 08:55:27 +0000

    rb_check values added

---
+++ bin/check-master.rb
@@ -0,0 +1,21 @@
+def check_master(services)
+  services.each { |x|
+    cmd = "lib/rb_nr_check " + x
+    value = `#{cmd}`
+    returned = $?
+
+    found = false
+    $check.each do |m|
+      if m["service"] == x
+        found = true
+        m["value"] = returned.to_s.split(' ')[3]
+      end
+    end
+    if !found
+      $check << {
+        "service" => x,
+        "value" => returned.to_s.split(' ')[3]
+      }
+    end
+  }
+end
