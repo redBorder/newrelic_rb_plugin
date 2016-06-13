af13f874369a201f067544bc7b8d276dc75eb9f3
Fran Rios <fcojriosbello@gmail.com>
2016-06-13 11:39:52 +0000

    added config

--- bin/check-master.rb
+++ bin/check-master.rb
@@ -1,9 +1,11 @@
 def check_master(services)
   services.each { |x|
     $logger.debug(x + ' service checked')
+    puts 'before'
     cmd = 'cat /tmp/rb_check.lock 2> /dev/null | xargs kill -9 2> /dev/null;
     rb_check -s ' + x + ' 2> /dev/null | grep FAILED | wc -l'
     value = `#{cmd}`
+    puts 'after'
     status = (value.strip != '0') ? 0 : 1
     # puts 'service is ' + x + ' value: ' + value + ' then status is ' + status.to_s
     found = false
