a019b2708d6b5817b89db6732e7a16d11df4f8c4
Fran Rios <fcojriosbello@gmail.com>
2016-06-15 14:28:03 +0000

    updated check

--- bin/check-master.rb
+++ bin/check-master.rb
@@ -1,26 +1,24 @@
-def check_master(services)
-  services.each { |x|
-    $logger.debug(x + ' service checked')
-    puts 'before'
-    cmd = 'cat /tmp/rb_check.lock 2> /dev/null | xargs kill -9 2> /dev/null;
-    rb_check -s ' + x + ' 2> /dev/null | grep FAILED | wc -l'
+def check_master(service)
+    $logger.debug(service + ' service checked')
+
+    cmd = 'lib/rb_nr_check ' + service
     value = `#{cmd}`
-    puts 'after'
-    status = (value.strip != '0') ? 0 : 1
-    # puts 'service is ' + x + ' value: ' + value + ' then status is ' + status.to_s
+    status = ($?.to_s.split(' ')[3] == '1') ? 0 : 1
+    puts 'service is ' + service + ' then status is ' + status.to_s
+
+
     found = false
 
     $check.each do |m|
-      if m['service'] == x
+      if m['service'] == service
         found = true
         m['value'] = status
       end
     end
     if !found
       $check << {
-        'service' => x,
+        'service' => service,
         'value' => status
       }
     end
-  }
 end
