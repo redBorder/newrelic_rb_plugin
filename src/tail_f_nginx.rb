afd55ba4f524307980401f4b5ed7d2d8cf6bac88
franrios <fcojriosbello@gmail.com>
2016-06-15 17:15:50 +0200

    renamed tail nginx

---
+++ src/tail_f_nginx.rb
@@ -0,0 +1,34 @@
+require 'file-tail'
+
+def nginx_parser(line, file)
+  unless line.nil?
+    unless line.match(/HTTP\/[1-9].[1-9]" ()?\d+/).to_s.split(" ")[1].nil?
+      status = line.match(/HTTP\/[1-9].[1-9]" ()?\d+/).to_s.split(" ")[1]
+    end
+    unless status.nil?
+      found = false
+      $nginx.each do |m|
+        if m["status"] == status
+          found = true
+          m["times"] += 1
+          m["ttl"] = 10
+        end
+      end
+      if !found
+        $nginx << {
+          "status" => status,
+          "times" => 1,
+          "ttl" => 10
+        }
+      end
+    end
+  end
+end
+
+def log_handler_nginx(filename)
+  File.open(filename, 'r') do |log|
+    log.extend(File::Tail)
+    log.backward(1)
+    log.tail { |line| nginx_parser(line, filename) }
+  end
+end
